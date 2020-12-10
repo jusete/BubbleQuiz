3a2275d9d4e25c09e3c40974541c6c635693bc3d
jusete <jusete@users.noreply.github.com>
2020-12-10 10:30:33 +0100

    Add files via upload

---
+++ TestVideo2.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestVideo2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var vp = gameObject.AddComponent<UnityEngine.Video.VideoPlayer>();
+        vp.url = "https://teleonuba.es/teleonuba/2020/12/09/15-vtr-cribados-masivos-palos-ok.mp4";
+
+        vp.isLooping = true;
+        vp.renderMode = UnityEngine.Video.VideoRenderMode.MaterialOverride;
+        vp.targetMaterialRenderer = GetComponent<Renderer>();
+        vp.targetMaterialProperty = "_MainTex";
+
+        vp.Play();
+    }
+   
+}
